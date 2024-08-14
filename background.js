d19fc014595128c321f1605707322f458ad1a2ee
Ian Dalmas <ian.dalmas@icloud.com>
2024-08-14 10:17:30 -0500

    make it simpler as an mvp

--- background.js
+++ background.js
@@ -1,23 +1,12 @@
-// background.js
-const redirects = {
-    "instagram.com": "https://docs.google.com/document/create",
-    "facebook.com": "https://trello.com/",
-    "twitter.com": "https://asana.com/",
-    "reddit.com": "https://www.coursera.org/",
-    "youtube.com": "https://www.duolingo.com/",
-    "netflix.com": "https://www.edx.org/",
-    // Add more redirects as needed
-  };
-  
-  chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-      const url = new URL(details.url);
-      for (const [unproductive, productive] of Object.entries(redirects)) {
-        if (url.hostname.includes(unproductive)) {
-          return {redirectUrl: productive};
-        }
-      }
-    },
-    {urls: ["<all_urls>"]},
-    ["blocking"]
-  );
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.action.disable(); // Disable the action button if present
+});
+
+chrome.declarativeNetRequest.onRuleRequest.addListener(
+  (details) => {
+    if (details.url.host === "googleusercontent.com/youtube.com/0" || details.url.host === "googleusercontent.com/youtube.com/1") {
+      return { redirectUrl: "https://www.notion.so" };
+    }
+  },
+  { urls: ["<all_urls>"] }
+);
