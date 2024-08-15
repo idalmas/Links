96af4169291b39218e223eebaab1476ccd440ad0
Ian Dalmas <ian.dalmas@icloud.com>
2024-08-14 20:01:27 -0500

    Update background.js

--- background.js
+++ background.js
@@ -3,6 +3,9 @@ chrome.webNavigation.onBeforeNavigate.addListener(
     if (details.url.includes("youtube.com")) {
       chrome.tabs.update(details.tabId, {url: "https://www.youtube.com/watch?v=lzMkFIw8ETM"});
     }
+    if (details.url.includes("x.com")) {
+      chrome.tabs.update(details.tabId, {url: "https://x.com/paulg/status/1823866572127220173"});
+    }
   },
   {
     url: [
